d73e68e702fd9579c3e1b52697925f58a3f08208
janghwal <jhp337037@gmail.com>
2025-04-17 18:05:37 +0900

    레벨 1-1: Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -26,13 +26,18 @@ public class AuthService {
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
 
+        // 1. 코드 개선 퀴즈 - Early Return
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+        }
+
         String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
 
         UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
-        if (userRepository.existsByEmail(signupRequest.getEmail())) {
-            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
-        }
+//        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+//            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+//        }
 
         User newUser = new User(
                 signupRequest.getEmail(),
