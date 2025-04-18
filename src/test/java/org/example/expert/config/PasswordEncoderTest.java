5021193057c2b21933810b90cef19ce3a4b3f534
janghwal <jhp337037@gmail.com>
2025-04-18 11:52:20 +0900

    레벨 3-1: 테스트 코드 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -14,13 +14,17 @@ class PasswordEncoderTest {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    void matches_메서드가_정상적으로_동작한다() {
+    void PasswordEncoder_matches_true() {
         // given
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+//        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // PasswordEncoder의 matches는 matches(String rawPassword, String encodedPassword)와 같은 형태로 되어있다.
+        // 첫번째 매개변수로 기존 비밀번호 즉 인코딩 되지 않은 비밀번호, 두번째 매개변수로 인코딩 되어 저장되어있는 비밀번호를 비교하는 함수이므로
+        // rawPassword와 encodedPassword가 알맞은 위치에 들어갈 수 있도록 두 매개 변수의 자리를 바꿔준다.
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
