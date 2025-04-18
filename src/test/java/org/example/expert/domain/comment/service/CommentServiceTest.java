42a67362e36578f6e08ac75f72926733644289ef
janghwal <jhp337037@gmail.com>
2025-04-18 16:44:53 +0900

    레벨 3-2: 테스트 코드 연습 - 2 case 2

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -5,6 +5,7 @@ import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
@@ -43,7 +44,14 @@ class CommentServiceTest {
         given(todoRepository.findById(anyLong())).willReturn(Optional.empty());
 
         // when
-        ServerException exception = assertThrows(ServerException.class, () -> {
+        // 어떤 todoId로 접근해도 findById에서 비어있는 Optional 을 리턴하기 때문에
+//        Todo todo = todoRepository.findById(todoId).orElseThrow(() ->
+//                new InvalidRequestException("Todo not found"));
+        // InvalidRequestException("Todo not found")가 발생한다.
+//        ServerException exception = assertThrows(ServerException.class, () -> {
+//            commentService.saveComment(authUser, todoId, request);
+//        });
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> {
             commentService.saveComment(authUser, todoId, request);
         });
 
